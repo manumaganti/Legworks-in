8c0fb0854e014872f52ee6eed32d3a94f7442f6f
Manu Maganti <manu.maganti@gmail.com>
2026-03-12 00:19:33 +0530

    made changes in header-component

--- src/app/controls/header-component/header-component.ts
+++ src/app/controls/header-component/header-component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { DropdownService } from '../../services/dropdown.service';
 import { Router } from '@angular/router';
+import { InfoFormComponent } from "../info-form-component/info-form-component";
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  standalone: true,
+  imports: [InfoFormComponent],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss',
 })
 export class HeaderComponent {
+
   isServicesOpen = false;
   isContactOpen = false;
+
+  showServiceModal = false;
+  selectedService = '';
 
   constructor(private dropdownService: DropdownService, private router: Router) {}
 
@@ -21,9 +27,18 @@ export class HeaderComponent {
     });
   }
 
-  hello() {
-    this.router.navigate(['/infoform']);
-  }
+// openServiceForm(service: string) {
+//   this.selectedService = service;
+//   this.showServiceModal = true;
+//   this.isServicesOpen = false;
+
+//   document.body.style.overflow = 'hidden';
+// }
+
+closeServiceForm() {
+  this.showServiceModal = false;
+  document.body.style.overflow = 'auto';
+}
 
   toggleServices() {
     this.isServicesOpen = !this.isServicesOpen;
@@ -45,4 +60,23 @@ export class HeaderComponent {
   hideServices() {
     this.isServicesOpen = false;
   }
+  scrollToAbout() {
+  const element = document.getElementById('about');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+
+}
+  scrollToHome() {
+  const element = document.getElementById('home');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+  openServiceForm() {
+  const element = document.getElementById('services');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
 }
