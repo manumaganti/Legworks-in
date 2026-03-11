86690d4a590ce5ed236e890f73139a3bb0c9a42d
Manu Maganti <manu.maganti@gmail.com>
2026-03-12 00:22:17 +0530

    made changes in module-component

--- src/app/controls/module-component/module-component.ts
+++ src/app/controls/module-component/module-component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header-component/header-component";
 import { BodyComponent } from "../body-component/body-component";
+import { VariousServicesComponent } from "../various-services-component/various-services-component";
+import { AboutUsComponent } from "../about-us-component/about-us-component";
+import { ParentComponent } from "../parent-component/parent-component";
 
 @Component({
   selector: 'app-module-component',
-  imports: [HeaderComponent, BodyComponent],
+  imports: [HeaderComponent, BodyComponent, VariousServicesComponent, AboutUsComponent, ParentComponent],
   templateUrl: './module-component.html',
   styleUrl: './module-component.scss',
 })
